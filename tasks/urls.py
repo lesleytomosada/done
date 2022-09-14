c51f9457a60803c9609dc9915a3cba57272a0f4c
Lesley <tomosadalesley@gmail.com>
2022-09-14 12:02:04 -0700

    fixed 8

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import TaskCreateView
+from .views import TaskCreateView, SelfTaskListView
 
-urlpatterns = [path("create/", TaskCreateView.as_view(), name="create_task")]
+urlpatterns = [
+    path("create/", TaskCreateView.as_view(), name="create_task"),
+    path("mine/", SelfTaskListView.as_view(), name="show_my_tasks"),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
+from asyncio import tasks
 from django.shortcuts import redirect
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from tasks.models import Task
@@ -15,3 +17,11 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.save()
         return redirect("list_projects")
+
+
+class SelfTaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "tasks/my_tasks.html"
+
+    def get_queryset(self):
+        return Task.objects.filter(assignee=self.request.user)
