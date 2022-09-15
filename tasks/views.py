0b9cb52064e3633ea8d34808d56c0d97c16e9965
Lesley <tomosadalesley@gmail.com>
2022-09-14 17:45:23 -0700

    Final submission

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import redirect
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -6,16 +5,21 @@ from django.urls import reverse_lazy
 
 from tasks.models import Task
 
-
 # Create your views here.
+
+
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     template_name = "tasks/create.html"
     fields = ["name", "start_date", "due_date", "project", "assignee"]
 
-    def form_valid(self, form):
-        form.save()
-        return redirect("list_projects")
+    def get_success_url(self):
+        return reverse_lazy("show_project", args=[self.object.project.id])
+
+
+# def form_valid(self, form):
+#     form.save()
+#     return redirect("list_projects")
 
 
 class SelfTaskListView(LoginRequiredMixin, ListView):
